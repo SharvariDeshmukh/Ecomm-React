4860ec7094b39b11e5baab85c4274d0beee3e19d
Sharvari Deshmukh <sharvarideshmukh475@gmail.com>
2019-12-26 23:57:04 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,41 +1,48 @@
-  
-import React from "react"
-import { Link } from "gatsby"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import HeroSection from "../components/reusable/HeroSection";
-import Infoblock from "../components/reusable/Infoblock";
-import Dualinfoblock from "../components/reusable/Dualinfoblock";
-import Teamphotosection from "../components/About/Teamphotosection";
-import SEO from "../components/seo"
-
-const AboutPage = ({data}) => (
-  <Layout>
-    <SEO title="Home"/>
-    <HeroSection
-    img={data.img.childImageSharp.fluid}
-      title="I write Code"
-      
-      heroclass="about-background"
-    
-    ></HeroSection>
-    
-    <Dualinfoblock heading="Message from the CEO" Imgsrc="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
-    <Infoblock heading="About Our Vision" btnCond="false"/>
-    <Teamphotosection/>
-  </Layout>
-)
-
-export const query = graphql`
-{  
-    img: file(relativePath: { eq: "about.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 400, maxHeight: 250) {
-             ...GatsbyImageSharpFluid_tracedSVG
-            }
-          }
-    }
-}`
-
-
-export default AboutPage
+import React from 'react'
+import Heading from "../reusable/Heading"
+export default function Contact() {
+    return (
+        <section className="py-3">
+            <Heading title="Contact Us"/>
+                <div className=".col-10 col-sm-8 mx-auto">
+                    <form action="https://formspree.io/sharvarideshmukh475@gmail.com" method="POST" >
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                name="name" 
+                                id="name" 
+                                placeholder="Your Name" 
+                                className = "form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                                placeholder="Your email" 
+                                className = "form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                type="mobile" 
+                                name="mobile" 
+                                id="mobile" 
+                                placeholder="Your mobile" 
+                                className = "form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <textarea
+                                type="text" 
+                                name="description" 
+                                id="description" 
+                                placeholder="Your message" 
+                                className = "form-control"/>
+                        </div>
+                        <button type="submit" 
+                        className="btn btn-outline-info btn-block">
+                        Submit</button>
+                    </form>
+                </div>
+        </section>
+    )
+}
